d682bcbbf50847a9d60ac8083f634c002aff3950
Ankit Kumar <63963175+ankitkumar0810@users.noreply.github.com>
2021-02-25 14:50:00 +0530

    Add files via upload

---
+++ Size.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Size {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Size of byte is " + Byte.SIZE);
+        System.out.println("Size of short is " + Short.SIZE);
+        System.out.println("Size of int is " + Integer.SIZE);
+        System.out.println("Size of long is " + Long.SIZE);
+        System.out.println("Size of float is " + Float.SIZE);
+        System.out.println("Size of double is " + Double.SIZE);
+        System.out.println("Size of char is " + Character.SIZE);
+        // System.out.println("Size of boolean is " + Boolean.SIZE);  NOT AVAILABLE WITH BOOLEAN
+    }
+    
+}
